9d08757b6a4e9c34d3b720274fb4fd60a1966408
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2022-10-01 11:02:47 -0500

    feat: dark mode

--- src/components/UserDropdown.tsx
+++ src/components/UserDropdown.tsx
@@ -4,38 +4,36 @@ import Link from 'next/link';
 import { handleSignOut } from '../utils/auth';
 
 // Icons
-import { IoExitOutline } from 'react-icons/io5';
-import { FaUserEdit } from 'react-icons/fa';
+import { IoExitOutline, IoSettingsOutline } from 'react-icons/io5';
+import { FaRegEdit } from 'react-icons/fa';
 
 function UserDropdown({ data }: { data: Session | null }) {
   const initials = userInitials(data?.user?.name);
 
   return (
     <div className="dropdown dropdown-end">
-      <label tabIndex={0}>
-        <div className="avatar cursor-pointer">
-          <div className="rounded-full">
-            {data?.user?.image ? (
-              <Image height={48} width={48} src={data.user.image} alt="" />
-            ) : (
-              <div className="bg-primary flex h-12 w-12 items-center justify-center">
-                <span className="text-xl tracking-wider text-white">
-                  {initials}
-                </span>
-              </div>
-            )}
-          </div>
+      <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+        <div className="w-10 rounded-full">
+          {data?.user?.image ? (
+            <Image height={48} width={48} src={data.user.image} alt="" />
+          ) : (
+            <div className="bg-primary flex h-10 w-10 items-center justify-center">
+              <span className="text-xl tracking-wider text-white">
+                {initials}
+              </span>
+            </div>
+          )}
         </div>
       </label>
       <ul
         tabIndex={0}
-        className="menu menu-compact dropdown-content bg-base-100 rounded-box w-48 p-2 shadow"
+        className="menu menu-compact dropdown-content bg-base-100 rounded-box w-44 p-2 shadow"
       >
         <li>
           <Link href="/profile">
             <div className="flex justify-between">
-              <FaUserEdit size={20} />
-              <span>Update Profile</span>
+              <FaRegEdit size={20} />
+              <span>Profile</span>
             </div>
           </Link>
         </li>
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,35 @@
 const AUTHENTICATED_ROUTES = ['/dashboard'];
 
-export { AUTHENTICATED_ROUTES };
+const THEMES = [
+  'light',
+  'dark',
+  // 'cupcake',
+  // 'bumblebee',
+  // 'emerald',
+  // 'corporate',
+  // 'synthwave',
+  // 'retro',
+  // 'cyberpunk',
+  // 'valentine',
+  // 'halloween',
+  // 'garden',
+  // 'forest',
+  // 'aqua',
+  // 'lofi',
+  // 'pastel',
+  // 'fantasy',
+  // 'wireframe',
+  // 'black',
+  // 'luxury',
+  // 'dracula',
+  // 'cmyk',
+  // 'autumn',
+  // 'business',
+  // 'acid',
+  // 'lemonade',
+  // 'night',
+  // 'coffee',
+  // 'winter',
+] as const;
+
+export { AUTHENTICATED_ROUTES, THEMES };
